35cf6a72508bdc9cae335c63cd43eab9c503a727
Nehomah Mora <nehomahm@gmail.com>
2019-01-18 11:40:46 -0700

    added purpose, audience and goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,5 +5,19 @@
 		<title>Nehomahs PWP</title>
 	</head>
 	<body>
+		<h1>Purpose</h1>
+		<p><strong>My PWP will help me accomplish informing people about the drum group so that they may be invited to more events.</strong></p>
+
+		<h1>Audience</h1>
+		<p><strong>The people accessing this site will be those involved with the Pow Wow circuit, event organizers, friends and family of the group members.</strong></p>
+
+		<h1>Goal</h1>
+		<ul>
+			<li><strong>To increase the groups web presence.</strong></li>
+			<li><strong>To provide links for resources related to the group.</strong></li>
+			<li><strong>To provide a contact point to be booked for events.</strong></li>
+			<li><strong>To showcase the groups outreach.</strong></li>
+			</ul>
+
 
 	</body>
