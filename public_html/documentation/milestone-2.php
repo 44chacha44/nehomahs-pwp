67c1d6a210ab1b87515534e90f8ed0007fb21fbf
Nehomah Mora <nehomahm@gmail.com>
2019-02-01 00:37:23 -0700

    completed content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -10,5 +10,10 @@
 
 		<p>My site will be one page and will consist of the following sections:</p>
 
-		<p><strong>Welcome:</strong></p>
+		<p><strong>Title with Image:</strong> The name of the group with a picture of them to introduce the user to who they will be learning about.</p>
+		<p><strong>About:</strong> This section will tell the story of the group and their mission. This will follow the Why/How/What model I
+											learned in prodev. The tone will be welcoming and kind to fit with the mission.</p>
+		<p><strong>Biographies:</strong> This section will have each core group members (4-6) biography and corresponding picture.</p>
+		<p><strong>Helpful Links:</strong> This section will link to their social media and music sharing pages.</p>
+		<p><strong>Contact:</strong> This will be a form to be filled out by user to be contacted by the group via email.</p>
 	</body>
