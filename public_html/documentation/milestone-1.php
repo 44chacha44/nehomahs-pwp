e6d1ca382ece8f820590c0a818865d0c2ce5bb1a
Nehomah Mora <nehomahm@gmail.com>
2019-01-31 23:51:17 -0700

    added linda persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -23,7 +23,7 @@
 			<img src="dyazzie.JPG" alt="dwayne">
 		</div>
 
-		<h1>Persona</h1>
+		<h1>Persona 1</h1>
 		<ul>
 			<li><strong>Name: Dwayne Yazzie</strong></li>
 			<li><strong>Age: 31</strong></li>
@@ -39,6 +39,25 @@
 			<li><strong>Frustrations: He has not received any invitations to practice with any of the drums he has sung with. Not all drum groups are online.</strong></li>
 			<li><strong>Goals: He wants to join a drum group that he can practice and sing with at pow wows. He wants information about each group to decide if they are a group he would get along with.</strong></li>
 			<li><strong>User Story: Dwayne wants to determine if this drum group would be one he would get along with based on similar interests and goals of the members.</strong></li>
+		</ul>
+
+		<h1>Persona 2</h1>
+		<ul>
+			<li><strong>Name: Linda Chavez</strong></li>
+			<li><strong>Age: 35</strong></li>
+			<li><strong>Ethnicity: Hispanic</strong></li>
+			<li><strong>Job: Arts and Cultural Services Program Manager</strong></li>
+			<li><strong>Location: Albuquerque, NM</strong></li>
+			<li><strong>Technology: iPhone XS, iPad Pro 12.9, AirPods. She relies heavily on her iPad to keep her on task and enjoys the syncing capabilities with her phone.</strong></li>
+			<li><strong>Hobbies: Vegetable garden every summer, reading world history books, attending cultural events in the city and surrounding areas</strong></li>
+			<li><strong>Favorite Websites: www.rachaelray.com, npr.org, youtube.com, library.si.edu</strong></li>
+			<li><strong>Behaviors and Attitudes: Linda likes to be prepared for her networking and non work events. She likes to research any and all information available online. Visual cues
+							are how she remembers best. She's always looking out for new groups to share their culture with the city.</strong></li>
+			<li><strong>Frustrations and Needs: A lot of groups are not found online. They usually don't carry business cards. She has invited groups that behaved terribly and likes to now have
+							some information to help decide if they should be considered. She was provided a large budget to have at least one event a week. She wants to provide new groups and new information each event but is
+							unable to have that variety due to lack of accessibility.</strong></li>
+			<li><strong>Goals: Linda would like to learn about the drum group and know how to contact them.</strong></li>
+			<li><strong>User Story: Linda needs more information about the drum group to determine if she wants to interview and hire them to be presenters.</strong></li>
 		</ul>
 		<p><strong></strong></p>
 
