dfc7cf011a790120297bdc9446bb25cbd64bdb69
Nehomah Mora <nehomahm@gmail.com>
2019-02-15 10:54:53 -0700

    added form validate script

---
+++ public_html/js/form-validate.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+
+	/**
+	 * jQuery Validate Function
+	 *
+	 * This function provides front-end validation for your form.
+	 * If all tests set up here pass, the form data is AJAX submitted
+	 * to the mailer.php file.
+	 *
+	 * @author Rochelle Lewis <rlewis37@cnm.edu>
+	 **/
+
+	/* begin validate function here */
+	$("#contact-form").validate({
+
+		// setup handling of form errors
+		debug: true,
+		errorClass: "alert alert-danger",
+		errorLabelContainer: "#output-area",
+		errorElement: "div",
+
+		// rules here define what is good or bad input
+		// each rule starts with the form input element's NAME attribute
+		rules: {
+			firstName: {
+				required: true
+			},
+			lastName: {
+				required: true
+			},
+			email: {
+				email: true,
+				required: true
+			},
+			phone: {
+				required: false
+			},
+			message: {
+				required: true,
+				maxlength: 2000
+			}
+		},
+
+		// error messages to display to the end user when rules above don't pass
+		messages: {
+			firstName: {
+				required: "U no like first name?"
+			},
+			lastName: {
+				required: "Y u have no last name?"
+			},
+			email: {
+				email: "Please enter a valid email address.",
+				required: "Please enter a valid email address."
+			},
+			message: {
+				required: "Please enter a message.",
+				maxlength: "2000 characters max."
+			}
+		},
+
+		// AJAX submit the form data to back end if rules pass
+		submitHandler: function(form) {
+			$("#contact-form").ajaxSubmit({
+				type: "POST",
+				url: $("#contact-form").attr("action"),
+
+				success: function(ajaxOutput) {
+					// clear the output area's formatting
+					$("#output-area").css("display", "");
+
+					// write the server's reply to the output area
+					$("#output-area").html(ajaxOutput);
+
+					// reset the form if it was successful
+					if($(".alert-success").length >= 1) {
+						$("#contact-form")[0].reset();
+					}
+				}
+			})
+		}
+
+	});/* end validate function here */
+
+});/*end document.ready()*/
