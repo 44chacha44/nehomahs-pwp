0f39d92255f1bcd6c6b5a54d2f1d2dcc64cb808f
Nehomah Mora <nehomahm@gmail.com>
2019-02-19 12:52:00 -0700

    added containers with rows for bios, changed breakpoint order

--- public_html/index.php
+++ public_html/index.php
@@ -25,6 +25,7 @@
 	</head>
 	<body>
 		<!-- navbar -->
+		<header>
 		<div id="background">
 			<div>
 				<nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -52,5 +53,95 @@
 
 			</div>
 		</div>
+		</header>
+		<div class="jumbotron jumbotron-fluid">
+			<div class="container">
+				<h1 class="display-4 text-center">SUNKA SKA SINGERS</h1>
+				<p class="lead text-center">ALBUQUERQUE, NEW MEXICO</p>
+			</div>
+		</div>
+		<div class="jumbotron jumbotron-fluid">
+			<div class="container">
+				<h2 class="display-4 text-center">
+				<img src="simple-medicine-wheel.jpg" class="img-fluid" alt="Responsive image"></h2>
+				<p class="lead text-center">A'ho! Our group was formed.....and then............and then.......................and then.................. something......................blah. ........................la..................................... de.................................................... da................................... blah............................................. de................................ lah........................................................... do....................................... re................................................. mi........................................ fa........................................................ so................................... ti.............................................................. do.</p>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+						<img src="simple-medicine-wheel.jpg" class="img-fluid" alt="Responsive image">
+				</div>
+				<div class="col-sm-9">
+						<div class="mx-auto m-2 p-5">
+							<p class="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+						</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row flex-column-reverse flex-md-row">
+				<div class="col-sm-9">
+					<div class="mx-auto m-2 p-5">
+						<p class="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+					</div>
+				</div>
+				<div class="col-sm-3">
+						<img src="simple-medicine-wheel.jpg" class="img-fluid" alt="Responsive image">
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<img src="simple-medicine-wheel.jpg" class="img-fluid" alt="Responsive image">
+				</div>
+				<div class="col-sm-9">
+					<div class="mx-auto m-2 p-5">
+						<p class="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row flex-column-reverse flex-md-row">
+					<div class="col-sm-9">
+						<div class="mx-auto m-2 p-5">
+							<p class="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<img src="simple-medicine-wheel.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<img src="simple-medicine-wheel.jpg" class="img-fluid" alt="Responsive image">
+				</div>
+				<div class="col-sm-9">
+					<div class="mx-auto m-2 p-5">
+						<p class="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row flex-column-reverse flex-md-row">
+					<div class="col-sm-9">
+						<div class="mx-auto m-2 p-5">
+							<p class="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<img src="simple-medicine-wheel.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				</div>
+			</div>
+		</div>
+
 	</body>
 </html>
