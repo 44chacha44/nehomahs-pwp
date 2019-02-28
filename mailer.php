00e34ead4d10d22cf0001bba4743dad6feb076d2
Nehomah Mora <nehomahm@gmail.com>
2019-02-28 14:58:01 -0700

    completed mailer.php scripts

--- mailer.php
+++ mailer.php
@@ -22,16 +22,48 @@ $recaptcha = new \ReCaptcha\ReCaptcha($secret);
 $resp = $recaptcha->verify($_POST["g-recaptcha-response"], $_SERVER["REMOTE_ADDR"]);
 
 try {
-		// if there's a reCAPTCHA error, throw an exception
-		// if (!$resp->isSuccess()) {
-		//			throw(new Exception("reCAPTCHA error!));
-		//	}
+	// if there's a reCAPTCHA error, throw an exception
+		if (!$resp->isSuccess()) {
+				throw(new Exception("reCAPTCHA error!"));
+		}
 
-		/**
-		 * Sanitize the inputs from the form: name email subject and message.
-		 * This assumes jQuery (NOT Angular!) will be AJAX submitting the form,
-		 * so we're using the $_POST superglobal.
-		 *
-		 **/
-		$name = filter_input(INPUT_POST)
+	/**
+	 * Sanitize the inputs from the form: name email subject and message.
+	 * This assumes jQuery (NOT Angular!) will be AJAX submitting the form,
+	 * so we're using the $_POST superglobal.
+	 *
+	 **/
+	$firstName = filter_input(INPUT_POST, "First name", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$lastName = filter_input(INPUT_POST, "Last name", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$emailAddress = filter_input(INPUT_POST, "Email address", FILTER_SANITIZE_EMAIL);
+	$message = filter_input(INPUT_POST, "Message", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	// create SendGrid object
+	$emailObject = new Mail();
+
+	/**
+	 * Attach the sender to the message.
+	 * This takes the form of an associative array where $email is the key for the real name.
+	 **/
+	$emailObject->setFrom($email, $firstName, $lastName);
+
+	/**
+	 * Attach the recipients to the message.
+	 * $MAIL_RECIPIENTS is set in mail-config.php
+	 **/
+	$recipients = $MAIL_RECIPIENTS;
+	$emailObject->addTo("sunkaskasingers@yahoo.com", "Sunka Ska");
+
+	// attach the message for the email
+	$emailObject->addMessage("text/plain", $message);
+
+	/**
+	 *  Using the SendGrid object from above call the send method and use the emailObject as an argument.
+	 **/
+	$response = $sendgrid->send($emailObject);
+
+	// report a successful send!
+	echo "<div class=\"alert alert-success\" role=\"alert\">Email sent successfully.</div>";
+} catch(\Exception $exception) {
+	echo "<div class= \"alert alert-danger\" role=\"alert\"><strong>Oops!</strong> Unable to send email: " . $exception->getMessage() . "</div>";
 }
